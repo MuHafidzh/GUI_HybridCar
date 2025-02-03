94f90662abc8394e2203a4c4b7a7b75a77afe5bc
MuHafidzh <hafidzhhabsyi04@gmail.com>
2025-02-03 10:56:09 +0700

    Remove build, devel, and .vscode from tracking

--- car_ws/build/udp_client/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hyridcar/car_ws/src/udp_client
-# Build directory: /home/hyridcar/car_ws/build/udp_client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
